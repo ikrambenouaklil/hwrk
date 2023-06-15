463b97275ba5acd0f196580135a288af552ececf
ikram <ikrambn2002@gmail.com>
2023-06-15 23:39:29 +0100

    comments

--- java.js
+++ java.js
@@ -1,10 +1,7 @@
-var butt= document.getElementById("gen"); 
-var con= document.getElementById("content"); 
-var aut= document.getElementById("author"); 
-const tab = new Array({
-  
+var butt= document.getElementById("gen"); //get the id of the generate button
+var con= document.getElementById("content");//get the id of quote 
+var aut= document.getElementById("author"); //get id of the author
 
-})
 butt.addEventListener('click', function(){
 con.textContent=' "Tegla3 est infini"';
 aut.textContent="-mikou-" ; 
