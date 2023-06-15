b83a20663daacbba184c1a95d5fb84d0de084261
ikram <ikrambn2002@gmail.com>
2023-06-15 23:56:53 +0100

    quots

--- java.js
+++ java.js
@@ -5,6 +5,24 @@ var aut= document.getElementById("author"); //get id of the author
 butt.addEventListener('click', function(){
 con.textContent=' "Tegla3 est infini"';
 aut.textContent="-mikou-" ; 
+var quotes = [
+    {
+        quote: ' "Tegla3 est infini"',
+        author: "-mikou vice president-"
+    },
+    {
+        quote: ' "chelkha msg"',
+        author: "-Sofiane chikhi ex RH leader-"
+},{
+    quote: ' "nanani nana"',
+    author: "-Adam design leader-"
+},{
+    quote: ' "tarararaaaaaaaa"',
+    author: "-Sara tae Ai trainings-"
+},{
+    quote: ' "Akhdm ll club ykhdmlk"',
+    author: "-Sofiane chikhi ex RH leader-"
+}]
 
 
 
