4a6e78f077d4ebe7ec40a4cbce3ff632227e6597
ikram <ikrambn2002@gmail.com>
2023-06-15 20:36:12 +0100

    int.

---
+++ java.js
@@ -0,0 +1,13 @@
+var butt= document.getElementById("gen"); 
+var con= document.getElementById("content"); 
+var aut= document.getElementById("author"); 
+const tab = new Array({
+  
+
+})
+butt.addEventListener('click', function(){
+con.textContent="Tegla3 est infinie";
+con.style.color="black";
+con.style.fontSize="20px"; 
+aut.textContent="mikou" ; 
+})
